64670128a5f833d41b1b2e3361239a6347920fd3
Ben Douthwaite <Ben.L.Douthwaite@gmail.com>
2021-04-26 22:43:13 +0100

    Content clear

--- content/blog/test/index.md
+++ content/blog/test/index.md
@@ -1,3 +1,9 @@
+---
+title: Test Page
+date: "2015-05-01T22:12:03.284Z"
+description: "It's a test page. Thrilling"
+---
+
 # Test post
 
 Content
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `BLD`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Ben Douthwaite`,
+      summary: `who lives and works in London copying useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
