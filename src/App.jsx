ec99ba242110ce4e12396aef7412860e349042d8
bld <bld.code@pm.me>
2024-01-15 22:54:02 +0000

    Remove custom logo for solid and add basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div id="container">
+        <div id="content">
+            <h1>Ben Douthwaite</h1>
+            <p>Software Engineer based in London, UK.</p>
+            <ul class="directory-list">
+                <li>Projects</li>
+                <li>Dev Blog</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
     </div>
   );
 }
