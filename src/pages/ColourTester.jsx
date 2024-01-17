54ab3553d6e7c49542bd3a718896e074d5aaa861
bld <bld.code@pm.me>
2024-01-17 22:55:43 +0000

    shorten lorem upsum

--- src/pages/ColourTester.jsx
+++ src/pages/ColourTester.jsx
@@ -19,11 +19,11 @@ export default function ColourTester() {
             <div id="content">
                 <h1>Colour Tester</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
                 <div class="secondary">
                     <p>
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                     </p>
                 </div>
                 <div id="controls">
